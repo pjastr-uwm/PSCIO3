22bac460bb27750c11aac334400f44285143e880
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-04-06 12:28:51 +0200

    4_2_14

---
+++ lab7/4_2_14/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+double * foo(int n)
+{
+    return (double*)malloc(n* sizeof(double));
+}
+
+int main()
+{
+    double * wsk = foo(4);
+    printf("%p\n", wsk );
+    free(wsk);
+    return 0;
+}

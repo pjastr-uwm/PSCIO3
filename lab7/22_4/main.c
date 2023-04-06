744de380f7c817f78254f7dd7431ab899b096775
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-04-06 13:47:13 +0200

    fix

--- lab7/22_4/main.c
+++ lab7/22_4/main.c
@@ -1,8 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+int nww(int a, int b)
+{
+    int t = a%b;
+    if (t==0)
+        return a;
+    return a* nww(b,t) /t;
+}
+
 int main()
 {
-    printf("Hello world!\n");
+    printf("%d\n", nww(12,60));
     return 0;
 }

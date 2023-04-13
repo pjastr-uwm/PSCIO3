7324b256cb8dfb3b97a1a9ccb5448489e22325d7
pjastr-uwm <114875756+pjastr-uwm@users.noreply.github.com>
2023-04-13 12:34:47 +0200

    5_2_1

--- lab8/5_2_1/main.c
+++ lab8/5_2_1/main.c
@@ -7,11 +7,20 @@ void wyczysc(char*napis)
     // napis[0] = 0;
 }
 
+void wyczysc2(wchar_t*napis)
+{
+    napis[0] = '\0';
+    // napis[0] = 0;
+}
 int main()
 {
     char n1[8] = "Olsztyn";
     printf("%s\n", n1);
     wyczysc(n1);
     printf("%s.\n", n1);
+    wchar_t n2[8] = L"Olsztyn";
+    wprintf(L"%s\n", n2);
+    wyczysc2(n2);
+    wprintf(L"%s.\n", n2);
     return 0;
 }
